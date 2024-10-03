fa38b711c8d9c15e73fa0d3a2da82d56de66be68
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-10-03 11:20:17 +0100

    Lesson4_P2

---
+++ src/main/webapp/js/shop.js
@@ -0,0 +1,56 @@
+﻿const initialState = {
+    page: 'home',
+    auth:{
+        token: null,
+        user: null
+    }
+};
+const AppContext = React.createContext(null);
+
+function reducer(state, action ){
+    switch(action.type){
+        case 'navigate':
+            window.location.hash = action.payload;
+            return {...state,
+        page: action.payload,}
+
+    }
+}
+
+
+function App(){
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+    React.useEffect(()=>{
+        let hash = window.location.hash;
+        if(hash.length > 1){
+            dispatch({type: "navigate", payload: hash.substring(1)});
+        }
+    }, []);
+    return <AppContext.Provider value={{state, dispatch}}>
+        <h1>Крамниця</h1>
+        <b onClick={() => dispatch({type: "navigate", payload: "home"})}>Домашня</b> <br></br>
+        <b onClick={() => dispatch({type: "navigate", payload: "cart"})}>Кошик</b>
+        {state.page === 'home' && <Home/>}
+        {state.page === 'cart' && <Cart/>}
+    </AppContext.Provider>
+}
+
+function Home(){
+    const {state,dispatch} = React.useContext(AppContext);
+    return <div>
+        <h2>Домашня!</h2>
+        <b onClick={() => dispatch({type: "navigate", payload: "cart"})}>До кошика</b>
+    </div>
+}
+
+function Cart() {
+    const {state,dispatch} = React.useContext(AppContext);
+    return <div>
+        <h2>Кошик!</h2>
+        <b onClick={() => dispatch({type: "navigate", payload: "home"})}>На домашню</b>
+    </div>
+}
+
+ReactDOM
+    .createRoot(document.getElementById("app-container"))
+    .render(<App />);
